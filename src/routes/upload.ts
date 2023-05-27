3bd060e070d0e3851b279ff7d8122144237619b6
wlLima <lima14.wl@gmail.com>
2023-05-27 16:20:37 -0300

    fix: removed spaces of regex expression

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -19,7 +19,7 @@ export async function uploadRoutes(app: FastifyInstance) {
       return reply.status(400).send()
     }
 
-    const mimeTypeRegex = /^(image | video)\/[a-zA-Z]+/
+    const mimeTypeRegex = /^(image|video)\/[a-zA-Z]+/
     const isValidFileFormat = mimeTypeRegex.test(upload.mimetype)
 
     if (!isValidFileFormat) {
